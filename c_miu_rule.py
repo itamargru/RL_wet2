7d60aab93ea27e2ae6aa87bc927f8be592591ff2
Itamar Gruber <itamar.gru@gmail.com>
2020-06-10 16:27:39 +0300

    added function to calculate V given policy

--- c_miu_rule.py
+++ c_miu_rule.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+action_number = 5
+states_number = 2**action_number
+jobs = np.arange(action_number)
+miu = np.array([0.6, 0.5, 0.3, 0.7, 0.1])
+cost = np.array([[1, 4, 6, 2, 9]])
+
 def initial_states():
     states = []
     for i in np.arange(2 ** action_number):
@@ -13,7 +19,10 @@ def initial_states():
 
 # V_p(s) = sum_s(cost) + miu(a) * V_p(s \ a) + (1 - miu(a)) * V_p(s)
 
-def calc_V_given_policy(policy, states, iteration=100):
+def calc_V_given_policy_numerical(policy, states, iteration=100):
+    P = create_P_matrix_given_policy(policy)
+    V = np.linalg.pinv(np.eye(states_number) - P) @ (states @ cost.T)
+
     V_prev = np.zeros(states_number)
     V_curr = np.zeros(states_number)
     for i in np.arange(iteration):
@@ -26,17 +35,33 @@ def calc_V_given_policy(policy, states, iteration=100):
     return V_curr
 
 
+def create_P_matrix_given_policy(policy, f):
+    P = np.zeros((states_number, states_number))
+    for state in np.arange(states_number):
+        P[state, f[state, policy[state]]] += miu[policy[state]]
+        P[state, state] += 1 - miu[policy[state]]
+    return P
 
-action_number = 5
-states_number = 2**action_number
-jobs = np.arange(action_number)
-miu = np.array([0.6, 0.5, 0.3, 0.7, 0.1])
-cost = np.array([[1, 4, 6, 2, 9]])
-states = initial_states()
+
+def calc_V_given_policy(policy, states, f):
+    P = create_P_matrix_given_policy(policy, f)
+    state_cost = states @ cost.T
+    pinv_prob = np.linalg.pinv(np.eye(states_number) - P)
+    V = pinv_prob @ state_cost
+    return V
 
 
 def clear_bit(num, bit):
     return num & (~(1 << bit))
+
+
+def matrix_next_state_given_state_policy():
+    f = np.zeros((states_number, action_number), dtype=int)
+    for state in range(states_number):
+        for action in range(action_number):
+            f[state, action] = clear_bit(state, action)
+    return f
+
 
 def random_policy():
     policy = [0]
@@ -46,11 +71,14 @@ def random_policy():
             k = np.random.randint(0, action_number)
             p = clear_bit(i, k)
         policy += [k]
+
     return policy
 
 # policy_0 = [0, 0, 0, 1, 0, 4, 2, 3, 0, 8, 2, 9, 8, 9, 10, 14, 0, 16, 2, 17, 16, 17, 6, 7, 8, 24, 10, 25, 20, 21, 28, 27]
 
 if __name__ == "__main__":
+    states = initial_states()
+    f = matrix_next_state_given_state_policy()
     policy = random_policy()
-    V = calc_V_given_policy(policy, states)
+    V = calc_V_given_policy(policy, states, f)
     pass
